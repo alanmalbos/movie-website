4ce1db45a86e97116373010767cdf625923be8d3
Alan Malbos <alanmalbos@gmail.com>
2018-10-03 21:36:01 -0300

    Add a class movie and test with a imput movie

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,1 +1,8 @@
 import media
+
+toy_story = media.Movie("Toy Story",
+	"A history of a boy and his toys become to life",
+	"https://pt.wikipedia.org/wiki/Toy_Story#/media/File:Movie_poster_toy_story.jpg",
+	"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print toy_story.storyline
--- media.py
+++ media.py
@@ -1,2 +1,6 @@
 class Movie():
-	
+	def __init__(self, title, storyline, poster_image_url, trailer_youtube):
+		self.title = title
+		self.storyline = storyline
+		self.poster_image_url = poster_image_url
+		self.trailer_youtube = trailer_youtube
