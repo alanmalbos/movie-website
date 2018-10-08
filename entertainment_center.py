1933b71bd119f63d7138352892872b3459fb41f9
Alan Malbos <alanmalbos@gmail.com>
2018-10-08 20:16:34 -0300

    Add a file to create the HTML to the movies site

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,28 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
 	"A history of a boy and his toys become to life",
-	"https://pt.wikipedia.org/wiki/Toy_Story#/media/File:Movie_poster_toy_story.jpg",
+	"/home/alanmalbos/version-control/movie-website/toy_story.jpg",
 	"https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 print toy_story.storyline
+
+avatar = media.Movie("Avatar",
+	"A marine on an alien planet",
+	"/home/alanmalbos/version-control/movie-website/avatar.jpg",
+	"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+print avatar.storyline
+
+the_godfather = media.Movie("The Godfather",
+	"A history of a mob family fight against their enemies",
+	"/home/alanmalbos/version-control/movie-website/the_godfather.jpg",
+	"https://www.youtube.com/watch?v=sY1S34973zA")
+
+movies = []
+movies.append(toy_story)
+movies.append(avatar)
+movies.append(the_godfather)
+
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,6 +1,11 @@
+from webbrowser import open
+
 class Movie():
-	def __init__(self, title, storyline, poster_image_url, trailer_youtube):
+	def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
 		self.title = title
 		self.storyline = storyline
 		self.poster_image_url = poster_image_url
-		self.trailer_youtube = trailer_youtube
+		self.trailer_youtube_url = trailer_youtube_url
+
+	def show_trailer(self):
+		open(self.trailer_youtube_url)
